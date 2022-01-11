980b250b6c2604d0c4ba43eb6fd418bab84319d6
Shin-sibainu <shincode0712@gmail.com>
2022-01-11 11:54:11 +0900

    setup

---
+++ main.js
@@ -0,0 +1,76 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "lil-gui";
+
+/**
+ * デバッグ(色つけるときに追加)
+ */
+
+/**
+ * 必須の3要素
+ */
+// Canvas
+const canvas = document.querySelector("#webgl");
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * ここからパーティクルを記述
+ */
+
+//カメラ制御
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * アニメーション
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+//ブラウザのリサイズ操作
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
