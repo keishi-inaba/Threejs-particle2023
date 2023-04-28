a9a15773dfe291466885e119c29d94687dd00d58
keishi.i <gajyumaru1103@gmail.com>
2023-04-28 16:33:47 +0900

    Three.jsを使ってサイト制作

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import * as dat from "lil-gui";
 /**
  * デバッグ(色つけるときに追加)
  */
+// const gui = new dat.GUI();
 
 /**
  * 必須の3要素
@@ -42,10 +43,46 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * ここからパーティクルを記述
  */
+const donutParticlesGeometry = new THREE.TorusGeometry(2.8, 0.7, 16, 100);
+
+const donutParticlesMaterial = new THREE.PointsMaterial({
+  size: 0.023,
+  color: "#39df0c"
+});
+
+const donutParticles = new THREE.Points(
+  donutParticlesGeometry,
+  donutParticlesMaterial
+);
+
+//パーティクルを追加
+const count = 5000;
+const particlesGeometry = new THREE.BufferGeometry();
+const positionArray = new Float32Array(count * 3);
+
+for(let i =0; i < count * 3; i++) {
+  positionArray[i] = (Math.random() -0.5) * 15;
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionArray, 3)
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.01,
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+
+scene.add(donutParticles, particles);
+
+// gui.addColor(donutParticlesMaterial, "color");
 
 //カメラ制御
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.enablePan = false;
 
 /**
  * アニメーション
@@ -54,6 +91,10 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  //カメラワーク
+  camera.position.x = Math.cos(elapsedTime * 0.5) * 6;
+  camera.position.z = Math.sin(elapsedTime * 0.5) * 6;
 
   controls.update();
   renderer.render(scene, camera);
